3a06808f2e96ed3aee7ce450744b4dc81dcf639d
Christopher Davis <cdavis9999@gmail.com>
2014-01-05 10:10:39 -0500

    Stub out client implementation

---
+++ src/me/christopherdavis/beanstalkc/DefaultClient.java
@@ -0,0 +1,173 @@
+// This file is part of me.christopherdavis.beanstalkc
+// Copyright: (c) 2014 Christopher Davis <http://christopherdavis.me>
+// License: MIT http://opensource.org/licenses/MIT
+
+package me.christopherdavis.beanstalkc;
+
+/**
+ * The default implementation of Client.
+ *
+ * @since   0.1
+ * @author  Christopher Davis <http://christopherdavis.me>
+ */
+class DefaultClient implements Client
+{
+    private static final int DEFAULT_DELAY = 0;
+    private static final int DEFAULT_TTR = 60; // 1 minute
+    private static final int DEFAULT_PRIORITY = 1024;
+
+    /**
+     * @see     Client#put
+     */
+    public Job put(int priority, int delay, int ttr, byte[] data) throws BeanstalkcException
+    {
+        return null;
+    }
+
+    /**
+     * @see     Client#put
+     */
+    public Job put(byte[] data) throws BeanstalkcException
+    {
+        return null;
+    }
+
+    /**
+     * @see     Client#use
+     */
+    public boolean use(String tube) throws BeanstalkcException
+    {
+        return false;
+    }
+
+    /**
+     * @see     Client#reserve
+     */
+    public Job reserve() throws BeanstalkcException
+    {
+        return null;
+    }
+
+    /**
+     * @see     Client#reserve
+     */
+    public Job reserve(int timeout) throws BeanstalkcException
+    {
+        return null;
+    }
+
+
+    /**
+     * @see     Client#delete
+     */
+    public boolean delete(Job job) throws BeanstalkcException
+    {
+        return false;
+    }
+
+
+    /**
+     * @see     Client#delete
+     */
+    public boolean delete(int job_id) throws BeanstalkcException
+    {
+        return false;
+    }
+
+    /**
+     * @see     Client#release
+     */
+    public boolean release(Job job, int priority, int delay) throws BeanstalkcException
+    {
+        return false;
+    }
+
+    /**
+     * @see     Client#release
+     */
+    public boolean release(int job_id, int priority, int delay) throws BeanstalkcException
+    {
+        return false;
+    }
+
+    /**
+     * @see     Client#release
+     */
+    public boolean release(Job job) throws BeanstalkcException
+    {
+        return false;
+    }
+
+    /**
+     * @see     Client#release
+     */
+    public boolean release(int job_id) throws BeanstalkcException
+    {
+        return false;
+    }
+
+    /**
+     * @see     Client#bury
+     */
+    public boolean bury(Job job, int priority) throws BeanstalkcException
+    {
+        return false;
+    }
+
+
+    /**
+     * @see     Client#bury
+     */
+    public boolean bury(int job_id, int priority) throws BeanstalkcException
+    {
+        return false;
+    }
+
+    /**
+     * @see     Client#bury
+     */
+    public boolean bury(Job job) throws BeanstalkcException
+    {
+        return false;
+    }
+
+    /**
+     * @see     Client#bury
+     */
+    public boolean bury(int job_id) throws BeanstalkcException
+    {
+        return false;
+    }
+
+    /**
+     * @see     Client#bury
+     */
+    public boolean touch(Job job) throws BeanstalkcException
+    {
+        return false;
+    }
+
+    /**
+     * @see     Client#touch
+     */
+    public boolean touch(int job_id) throws BeanstalkcException
+    {
+        return false;
+    }
+
+    /**
+     * @see     Client#touch
+     */
+    public int watch(String tube) throws BeanstalkcException
+    {
+        return 0;
+    }
+
+    /**
+     * @see     Client#ignore
+     */
+    public int ignore(String tube) throws BeanstalkcException
+    {
+        return 0;
+    }
+}
