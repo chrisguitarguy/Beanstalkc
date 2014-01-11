45abf9a9c87de9e105177554a17b450f745d6905
Christopher Davis <cdavis9999@gmail.com>
2014-01-10 22:30:44 -0600

    Introduce AbstractWatchingCommand

---
+++ src/me/christopherdavis/beanstalkc/command/AbstractWatchingCommand.java
@@ -0,0 +1,34 @@
+// This file is part of me.christopherdavis.beanstalkc
+// Copyright: (c) 2014 Christopher Davis <http://christopherdavis.me>
+// License: MIT http://opensource.org/licenses/MIT
+
+package me.christopherdavis.beanstalkc.command;
+
+import java.io.InputStream;
+import java.io.IOException;
+import me.christopherdavis.beanstalkc.BeanstalkcException;
+
+/**
+ * An abstract base class for Watch and Ignore commands -- anything that returns
+ * a WATCHING response.
+ *
+ * @since   0.1
+ * @author  Christopher Davis <http://christopherdavis.me>
+ */
+abstract class AbstractWatchingCommand extends AbstractCommand<Integer>
+{
+    /**
+     * @see     AbstractCommand#readResponse
+     * @return  < 0 if an error occured otherwise the number of tubes
+     *          being watch
+     */
+    @Override
+    protected Integer readResponse(String[] first_line, InputStream in) throws BeanstalkcException, IOException
+    {
+        if (first_line[0].equals("WATCHING") && first_line.length > 1) {
+            return parseInt(first_line[1], "watching number");
+        }
+
+        return -1;
+    }
+}
