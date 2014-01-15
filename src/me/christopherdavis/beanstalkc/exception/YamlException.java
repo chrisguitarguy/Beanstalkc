f9fd1f20b6e9ca366dda2f07d2481ba0936eaa55
Christopher Davis <cdavis9999@gmail.com>
2014-01-14 21:36:48 -0500

    Introduce YamlParser and AbstractYamlParser

---
+++ src/me/christopherdavis/beanstalkc/exception/YamlException.java
@@ -0,0 +1,31 @@
+// This file is part of me.christopherdavis.beanstalkc
+// Copyright: (c) 2014 Christopher Davis <http://christopherdavis.me>
+// License: MIT http://opensource.org/licenses/MIT
+
+package me.christopherdavis.beanstalkc.exception;
+
+import me.christopherdavis.beanstalkc.BeanstalkcException;
+
+/**
+ * Thrown when there's trouble parsing a YAML response.
+ *
+ * @since   0.1
+ * @author  Christopher Davis <http://christopherdavis.me>
+ */
+public class YamlException extends BeanstalkcException
+{
+    public YamlException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public YamlException(String msg, Throwable cause)
+    {
+        super(msg, cause);
+    }
+
+    public YamlException(String msg)
+    {
+        super(msg);
+    }
+}
---
+++ test/unit/me/christopherdavis/beanstalkc/command/AbstractYamlParserTest.java
@@ -0,0 +1,88 @@
+// This file is part of me.christopherdavis.beanstalkc
+// Copyright: (c) 2014 Christopher Davis <http://christopherdavis.me>
+// License: MIT http://opensource.org/licenses/MIT
+
+package me.christopherdavis.beanstalkc.command;
+
+import java.io.OutputStream;
+import org.junit.Test;
+import org.junit.Assert;
+import me.christopherdavis.beanstalkc.BeanstalkcException;
+
+public class AbstractYamlParserTest
+{
+    @Test
+    public void testWithBeanstalkcThrowingParse()
+    {
+        final BeanstalkcException e = new BeanstalkcException("Test Exception");
+
+        YamlParser<String> parser = new AbstractYamlParser<String>() {
+            @Override
+            protected String createOutput()
+            {
+                return new String();
+            }
+
+            @Override
+            public void handleLine(String line, String out) throws Exception
+            {
+                throw e;
+            }
+        };
+
+        try {
+            parser.parse("test".getBytes());
+        } catch (Exception caught) {
+            Assert.assertSame(e, caught);
+        }
+    }
+
+    @Test
+    public void testWithNonBeanstalkcException()
+    {
+        final Exception e = new Exception("Test Exception");
+
+        YamlParser<String> parser = new AbstractYamlParser<String>() {
+            @Override
+            protected String createOutput()
+            {
+                return new String();
+            }
+
+            @Override
+            public void handleLine(String line, String out) throws Exception
+            {
+                throw e;
+            }
+        };
+
+        try {
+            parser.parse("test".getBytes());
+        } catch (Exception caught) {
+            Assert.assertTrue(caught instanceof BeanstalkcException);
+            Assert.assertSame(e, caught.getCause());
+        }
+    }
+
+    @Test
+    public void testWithoutException() throws Exception
+    {
+        final String s = new String("test");
+
+        YamlParser<String> parser = new AbstractYamlParser<String>() {
+            @Override
+            protected String createOutput()
+            {
+                return s;
+            }
+
+            @Override
+            public void handleLine(String line, String out) throws Exception
+            {
+                // noop
+            }
+        };
+
+        Assert.assertSame(s, parser.parse("test".getBytes()));
+    }
+}
