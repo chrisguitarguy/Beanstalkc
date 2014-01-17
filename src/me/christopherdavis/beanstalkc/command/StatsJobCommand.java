eff764736854cc72302a0db010ebdd0796da6ccd
Christopher Davis <cdavis9999@gmail.com>
2014-01-16 21:00:18 -0500

    Introduce the stats commands

---
+++ src/me/christopherdavis/beanstalkc/command/StatsJobCommand.java
@@ -0,0 +1,47 @@
+// This file is part of me.christopherdavis.beanstalkc
+// Copyright: (c) 2014 Christopher Davis <http://christopherdavis.me>
+// License: MIT http://opensource.org/licenses/MIT
+
+package me.christopherdavis.beanstalkc.command;
+
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.Map;
+import me.christopherdavis.beanstalkc.BeanstalkcException;
+
+/**
+ * Send a stats-job command to the server.
+ *
+ * @since   0.1
+ * @author  Christopher Davis <http://christopherdavis.me>
+ */
+public class StatsJobCommand extends AbstractStatsCommand<Map<String, String>>
+{
+    private int job_id;
+
+    public StatsJobCommand(int job_id)
+    {
+        this.job_id = job_id;
+    }
+
+    /**
+     * @see     AbstractCommand#sendRequest
+     */
+    @Override
+    protected void sendRequest(OutputStream out) throws BeanstalkcException, IOException
+    {
+        out.write(String.format(
+            "stats-job %d\r\n",
+            job_id
+        ).getBytes());
+    }
+
+    /**
+     * @see     AbstractStatsCommand#getParser
+     */
+    @Override
+    protected YamlParser<Map<String, String>> getParser()
+    {
+        return new YamlDictParser();
+    }
+}
---
+++ test/unit/me/christopherdavis/beanstalkc/command/StatsCommandTest.java
@@ -0,0 +1,44 @@
+// This file is part of me.christopherdavis.beanstalkc
+// Copyright: (c) 2014 Christopher Davis <http://christopherdavis.me>
+// License: MIT http://opensource.org/licenses/MIT
+
+package me.christopherdavis.beanstalkc.command;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.Assert;
+import me.christopherdavis.beanstalkc.BeanstalkcException;
+
+public class StatsCommandTest
+{
+    final private byte[] expected = "stats\r\n".getBytes();
+    private StatsCommand cmd = new StatsCommand();
+
+    @Test
+    public void testWithOkResponse() throws Exception
+    {
+        final String yaml = "one: value\ntwo: value2";
+        final byte[] yaml_b = yaml.getBytes();
+
+        InputStream in = new ByteArrayInputStream(String.format(
+            "OK %d\r\n%s\r\n",
+            yaml_b.length,
+            yaml
+        ).getBytes());
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        Map<String, String> res = cmd.execute(in, out);
+
+        Assert.assertArrayEquals(expected, out.toByteArray());
+        Assert.assertNotNull(res);
+        Assert.assertTrue(res.containsKey("one"));
+        Assert.assertEquals("value", res.get("one"));
+        Assert.assertTrue(res.containsKey("two"));
+        Assert.assertEquals("value2", res.get("two"));
+    }
+}
